22f53559dd12dd5d1c6163e0150310d1d1d29090
jahlgren <jukka.ahlgren@gmail.com>
2020-03-26 14:34:03 +0200

    Heroku build

---
+++ build/precache-manifest.2872f6b803bb0a6bfb570655f87c5742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee08278155b5b548c9bf8470ae548d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90eb7fdc1fe368933616",
+    "url": "/static/css/main.3aebfd33.chunk.css"
+  },
+  {
+    "revision": "f86e109211db4a80c4c8",
+    "url": "/static/js/2.419b6edb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.419b6edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90eb7fdc1fe368933616",
+    "url": "/static/js/main.352eb254.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan
   .token('body', req => JSON.stringify(req.body))
