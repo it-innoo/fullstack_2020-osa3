af8bfde9a7bbe60e377eca62bb3c077663983ac8
jahlgren <jukka.ahlgren@gmail.com>
2020-03-26 11:45:42 +0200

    3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import morgan from 'morgan'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan
   .token('body', req => JSON.stringify(req.body))
