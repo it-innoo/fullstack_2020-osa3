e54a1c6e25f31865520550720ebcd68bd54c60fd
jahlgren <jukka.ahlgren@gmail.com>
2020-03-26 13:03:24 +0200

    Back to cjs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
